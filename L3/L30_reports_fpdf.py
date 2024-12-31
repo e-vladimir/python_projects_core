2608b4ae64c794fa43f7fa2e6d8f7e1cb23c4e5f
e_vladimir <79600069487@ya.ru>
2024-12-31 23:36:28 +0300

    + Реализация PDF-Генератора + Шрифты Noto - Шрифты IBM Plex

--- L3/L30_reports_fpdf.py
+++ L3/L30_reports_fpdf.py
@@ -1,8 +1,77 @@
 # ГЕНЕРАТОР ОТЧЁТОВ НА БАЗЕ FPDF
-# 24 сен 2024
+# 30 дек 2024
 
-from pathlib import Path
-from fpdf    import FPDF, YPos
+import enum
+
+from   dataclasses import dataclass
+from   datetime    import datetime
+from   pathlib     import Path
+from pprint import pprint
+
+from fpdf import Align, FPDF, FontFace
+from fpdf.enums import CellBordersLayout, TableBordersLayout, VAlign
+from fpdf.table import Row
+from wx.lib.sized_controls import border
+
+
+class BLOCKS(enum.Enum):
+	""" Блоки документа """
+	HEADER      = enum.auto()
+	FOOTER      = enum.auto()
+	H1          = enum.auto()
+	H2          = enum.auto()
+	H3          = enum.auto()
+	TEXT        = enum.auto()
+	CODE        = enum.auto()
+	TABLE       = enum.auto()
+	DESCRIPTION = enum.auto()
+
+
+class FORMATS_PAGE(enum.StrEnum):
+	""" Формат бумаги """
+	A3 = "A3"
+	A4 = "A4"
+
+
+@dataclass
+class T30_Margins:
+	l: float = 100.0
+	r: float = 10.0
+	t: float = 15.0
+	b: float = 15.0
+
+
+@dataclass
+class T30_InformationDocument:
+	title   : str      = ""
+	author  : str      = ""
+	edition : int      = 1
+	date    : datetime = datetime.now()
+
+
+@dataclass
+class T30_Color:
+	r: int = 0
+	g: int = 0
+	b: int = 0
+
+	def FromRGB(self, r: int, g: int = None, b: int = None):
+		""" Преобразование из RGB """
+		self.r = r
+		self.g = g or r
+		self.b = b or r
+
+	def ToRGB(self) -> tuple[int, int, int]:
+		""" Преобразование в RGB """
+		return self.r, self.g, self.b
+
+	def IsWhite(self) -> bool:
+		""" Проверка белого оттенка """
+		return sum(self.ToRGB()) > 759
+
+	def IsBlack(self) -> bool:
+		""" Проверка тёмного оттенка """
+		return sum(self.ToRGB()) < 6
 
 
 class C30_ProcessorReportsFpdf2(FPDF):
@@ -16,177 +85,237 @@ class C30_ProcessorReportsFpdf2(FPDF):
 
 	# Модель данных
 	def Init_00(self):
-		self._margin_l    : float = 30.0
-		self._margin_r    : float = 10.0
-		self._margin_t    : float = 10.0
-		self._margin_b    : float = 10.0
+		self._margins_page  : T30_Margins             = T30_Margins(30, 15, 15, 15)
+		self._info_document : T30_InformationDocument = T30_InformationDocument()
 
-		self._header_l    : str   = ""
-		self._header_r    : str   = ""
-		self._header_b    : str   = ""
-
-		self._size_header : float = 16.0
-		self._size_text   : float = 9.0
-		self._size_H1     : float = 14
-		self._size_H2     : float = 12
-		self._size_H3     : float = 10
-
-		self._padding_header : float = 10
+		self._processing_h1 : str                     = ""
+		self._processing_h2 : str                     = ""
+		self._processing_h3 : str                     = ""
 
 	# Модель событий
 	pass
 
 	# Механика данных
+	def SwitchColors(self, block: BLOCKS):
+		""" Смена цвета """
+		match block:
+			case BLOCKS.H1:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(255)
+
+			case BLOCKS.H2:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(255)
+
+			case BLOCKS.H3:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(255)
+
+			case BLOCKS.TEXT:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(255)
+
+			case BLOCKS.DESCRIPTION:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(255)
+
+			case BLOCKS.TABLE:
+				self.set_text_color(  0)
+				self.set_draw_color(180)
+				self.set_fill_color(255)
+
+			case BLOCKS.CODE:
+				self.set_text_color(  0)
+				self.set_draw_color(200)
+				self.set_fill_color(230)
+
+	def SwitchFont(self, block: BLOCKS):
+		""" Смена шрифта """
+		match block:
+			case BLOCKS.H1         : self.set_font("Sans", "B", 16)
+			case BLOCKS.H2         : self.set_font("Sans", "B", 12)
+			case BLOCKS.H3         : self.set_font("Sans", "B", 10)
+			case BLOCKS.TEXT       : self.set_font("Sans",  "",  9)
+			case BLOCKS.CODE       : self.set_font("Mono",  "",  8)
+			case BLOCKS.DESCRIPTION: self.set_font("Sans", "I",  8)
+			case BLOCKS.TABLE      : self.set_font("Mono",  "",  8)
+
+	# Механика управления: Шрифты
 	def LoadFonts(self, path_fonts: Path):
 		""" Загрузка шрифтов """
-		self.add_font("R",   "", f"{path_fonts.joinpath("IBM_Plex_Mono-R.ttf")}")
-		self.add_font("R",  "B", f"{path_fonts.joinpath("IBM_Plex_Mono-B.ttf")}")
-		self.add_font("R",  "I", f"{path_fonts.joinpath("IBM_Plex_Mono-I.ttf")}")
-		self.add_font("R", "BI", f"{path_fonts.joinpath("IBM_Plex_Mono-BI.ttf")}")
+		self.add_font("Sans",   "", f"{path_fonts.joinpath("NotoSans-Regular.ttf")}")
+		self.add_font("Sans",  "B", f"{path_fonts.joinpath("NotoSans-Bold.ttf")}")
+		self.add_font("Sans",  "I", f"{path_fonts.joinpath("NotoSans-Italic.ttf")}")
 
-	# Механика управления
-	def AppendPage(self):
-		""" Вставка страницы """
-		self.add_page(orientation="P", format="A4")
-		self.set_margins(self._margin_l, self._margin_t, self._margin_r)
+		self.add_font("Mono",   "", f"{path_fonts.joinpath("NotoSansMono-Regular.ttf")}")
+		self.add_font("Mono",  "B", f"{path_fonts.joinpath("NotoSansMono-Bold.ttf")}")
+		self.add_font("Mono",  "I", f"{path_fonts.joinpath("NotoSansMono-Regular.ttf")}")
 
-		self.AppendHeader()
-		self.AppendFooter()
+	# Механика управления: Страница
+	def NewPage(self, flag_rotate: bool = False, format_page: FORMATS_PAGE = FORMATS_PAGE.A4):
+		""" Новая страница страницы """
+		self.add_page(orientation = "L" if flag_rotate else "P",
+		              format      = format_page.value)
 
-		self.set_auto_page_break(True, self._margin_b)
+		self.set_margins(self._margins_page.l, self._margins_page.t, self._margins_page.r)
+		self.set_auto_page_break(True, self._margins_page.b)
 
-	def AppendText(self):
-		""" Вставка текста """
-		pass
+	# Механика управления: Блоки
+	def AppendH1(self, h1: str = ""):
+		""" Добавление заголовка 1 """
+		if not h1                       : return
+		if     h1 == self._processing_h1: return
 
-	def AppendImage(self):
-		""" Вставка изображения """
-		pass
+		self._processing_h1 = h1
 
-	def AppendImageGrid(self):
-		""" Вставка сетки изображений """
-		pass
+		self.SwitchFont(BLOCKS.H1)
+		self.SwitchColors(BLOCKS.H1)
 
-	def AppendTable(self, header: list[str], data: list[list[str]], column_sizes : list[int] = [], column_aligns: list[str] = []):
-		""" Вставка таблицы данных """
-		self.set_font("R", "", self._size_text)
-		self.set_text_color(255, 255, 255)
-		self.set_fill_color(26, 26, 26)
+		self.set_x(self._margins_page.l)
 
-		cell_h_base = self._size_text * 0.35 + 1
+		self.multi_cell(w     = 0.00,
+		                align = Align.J,
+		                text  = h1.upper())
 
-		widths       : list[float] = [(self.w - self._margin_l - self._margin_r) / len(header)] * len(header)
-		if column_sizes: widths    = [(self.w - self._margin_l - self._margin_r) * column_size / 100 for column_size in column_sizes]
+		self.ln(5)
 
-		aligns       : list[str]   = ["J"] * len(header)
-		if column_aligns: aligns   = column_aligns.copy()
+	def AppendH2(self, h2: str = ""):
+		""" Добавление заголовка 2 """
+		if not h2                       : return
+		if     h2 == self._processing_h2: return
 
-		for column_index, header_item in enumerate(header):
-			cell_w : float = widths[column_index]
+		self.ln(5)
 
-			self.multi_cell(w = cell_w, h=cell_h_base, text=header_item, border=0, align=aligns[column_index], fill=True, new_y=YPos.LAST)
+		self._processing_h2 = h2
 
-		self.ln()
-		self.set_draw_color(200, 200, 200)
-		self.set_text_color(  0,   0,   0)
-		for row in data:
-			cell_h_max : int = 1
+		self.SwitchFont(BLOCKS.H2)
+		self.SwitchColors(BLOCKS.H2)
 
-			for column_index, subdata in enumerate(row):
-				cell_w : float = widths[column_index]
-				text_w : float = self.get_string_width(subdata)
+		self.set_x(self._margins_page.l)
 
-				cell_h_max = max(cell_h_max, 1 + int(text_w // (cell_w - 1)))
+		self.multi_cell(w     = 0.00,
+		                align = Align.J,
+		                text  = h2.upper())
 
-			for column_index, subdata in enumerate(row):
-				cell_w : float = widths[column_index]
-				text_w : float = self.get_string_width(subdata)
+		self.ln(5)
 
-				self.multi_cell(w=cell_w, h=cell_h_base * (cell_h_max - text_w // (cell_w - 1)), text=subdata, border="B", align=aligns[column_index], fill=False, new_y=YPos.LAST)
+	def AppendH3(self, h3: str = ""):
+		""" Добавление заголовка 3 """
+		if not h3                       : return
+		if     h3 == self._processing_h3: return
 
-			self.ln()
+		self._processing_h3 = h3
 
-		self.ln()
+		self.SwitchFont(BLOCKS.H3)
+		self.SwitchColors(BLOCKS.H3)
 
-	def AppendHeader(self):
-		""" Вставка верхнего колонтитула """
-		if self._header_l:
-			self.set_font("R", "B", self._size_header)
-			self.set_xy(self._margin_l, self._margin_t)
+		self.set_x(self._margins_page.l)
 
-			self.cell(text=self._header_l)
+		self.multi_cell(w     = 0.00,
+		                align = Align.J,
+		                text  = h3.upper())
 
-		if self._header_r:
-			self.set_font("R", "B", self._size_header)
-			self.set_xy(self.w - self.get_string_width(self._header_r) - self._margin_r, self._margin_t)
+		self.ln(2)
 
-			self.cell(text=self._header_r)
+	def AppendText(self, h1: str = "", h2: str = "", h3: str = "", text: str | list[str] = ""):
+		""" Добавление текста """
+		if not text: return
 
-		if self._header_b:
-			self.set_font("R", "B", self._size_header * 0.75)
-			self.set_xy(self._margin_l, self._margin_t + self.font_size + self.font_size * 0.75)
+		self.AppendH1(h1)
+		self.AppendH2(h2)
+		self.AppendH3(h3)
 
-			self.cell(text=self._header_b)
+		self.SwitchFont(BLOCKS.TEXT)
+		self.SwitchColors(BLOCKS.TEXT)
 
-		self.ln()
+		self.set_x(self._margins_page.l)
 
-	def AppendFooter(self):
-		""" Вставка нижнего колонтитула """
-		pass
+		self.multi_cell(w     = 0.00,
+		                align = Align.J,
+		                text  = text if type(text) is str else '\n\n'.join(text))
 
-	def AppendH1(self, text: str):
-		""" Вставка заголовка 1 """
-		self.set_font("R", "B", self._size_H1)
+		self.ln(5)
 
-		self.ln()
-		self.ln()
+	def AppendCode(self, code: str | list[str] = ""):
+		""" Добавление кода """
+		if not code: return
 
-		w : float = self.w
-		w        -= self._margin_l
-		w        -= self._margin_r
+		self.SwitchFont(BLOCKS.CODE)
+		self.SwitchColors(BLOCKS.CODE)
 
-		self.multi_cell(w, text=text)
-		self.ln()
+		self.set_x(self._margins_page.l)
 
-	def AppendH2(self, text: str):
-		""" Вставка заголовка 2 """
-		self.set_font("R", "B", self._size_H2)
+		self.multi_cell(w     = 0.00,
+		                align = Align.J,
+		                text  = code if type(code) is str else '\n\n'.join(code),
+		                fill  = True,
+		                padding=2)
 
-		self.ln()
-		self.ln()
+		self.ln(5)
 
-		w : float = self.w
-		w        -= self._margin_l
-		w        -= self._margin_r
+	def AppendList(self, items: list[str], flag_numeric: bool = False):
+		""" Добавление списка """
+		self.SwitchFont(BLOCKS.TEXT)
+		self.SwitchColors(BLOCKS.TEXT)
 
-		self.multi_cell(w, text=text)
-		self.ln()
+		for idx, item in enumerate(items):
+			marker: str = f'{idx + 1}.' if flag_numeric else '•'
 
-	def AppendH3(self, text: str):
-		""" Вставка заголовка 3 """
-		self.set_font("R", "B", self._size_H3)
+			self.set_x(self._margins_page.l + 5)
+			self.multi_cell(w        = 0.00,
+			                align    = Align.J,
+			                text     = f"{marker} {item}")
+			self.ln(2)
 
 		self.ln()
-		self.ln()
 
-		w : float = self.w
-		w        -= self._margin_l
-		w        -= self._margin_r
+	def AppendTable(self, description: str = "", header: list[str] = [], data: list = [], sizes: list[int] = [], aligns: list[Align] = []):
+		""" Добавление таблицы """
+		if not header: return
+		if not data  : return
 
-		self.multi_cell(w, text=text)
-		self.ln()
+		column_sizes  : list[float] = [(self.epw - sum(sizes)) / (len(header) - len(sizes))] * len(header)
+		column_aligns : list[Align] = [Align.L] * len(header)
+
+		for idx, size  in enumerate(sizes) : column_sizes[idx]  = size
+		for idx, align in enumerate(aligns): column_aligns[idx] = align
+
+		self.SwitchFont(BLOCKS.DESCRIPTION)
+		self.SwitchColors(BLOCKS.DESCRIPTION)
+
+		self.multi_cell(w     = 0.00,
+		                text  = description,
+		                align = Align.R)
+
+		self.SwitchFont(BLOCKS.TABLE)
+		self.SwitchColors(BLOCKS.TABLE)
+
+		with self.table(borders_layout  = TableBordersLayout.SINGLE_TOP_LINE,
+		                col_widths      = column_sizes,
+		                text_align      = column_aligns,
+		                repeat_headings = True,
+		                line_height     = self.font_size * 1.25,
+		                padding         = 1,
+		                align           = Align.R,
+		                v_align         = VAlign.T) as table:
+			for data_row in [header] + data:
+				row : Row = table.row()
+
+				for data_cell in data_row:
+					row.cell(text   = data_cell,
+					         border = "BOTTOM")
+
+		self.ln(5)
 
 	# Логика данных
-	def ExportReportToPdf(self, file_path: Path):
-		""" Экспорт отчёта в PDF-формате """
+	def SaveToPdf(self, file_path: Path):
+		""" Сохранение в PDF-файл """
 		self.output(f"{file_path}")
 
-	# Логика управления: Страница
-	def AppendTitlePage(self):
-		""" Вставка титульной страницы """
-		pass
-
-	def AppendContentsPage(self):
-		""" Вставка оглавления """
-		pass
-
+	# Логика управления
+	pass
