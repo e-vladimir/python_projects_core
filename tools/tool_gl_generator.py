3aaefa53db54cbcb6348f2927b120f53d9966920
e_vladimir <79600069487@ya.ru>
2025-02-12 17:30:10 +0300

    * Переписан генератор L-файлов

--- tools/tool_gl_generator.py
+++ tools/tool_gl_generator.py
@@ -1,168 +1,142 @@
 # УТИЛИТЫ: ГЕНЕРАТОР GL-ФАЙЛОВ И КЛАССОВ
-# 01 фев 2025
+# 11 фев 2025
 
 import datetime
 import enum
 
-from   os      import mkdir
-from   pathlib import Path
+from   dataclasses import dataclass
+from os import mkdir
+from   pathlib     import Path
 
 
-class LS(enum.Enum):
-	""" Каталог L-уровней """
-	def __init__(self, code: int, description: str):
-		self.level       = code
-		self.description = description
-
-	L0 = ( 0, "Источник данных")
-	L1 = (10, "Библиотека")
-	L2 = (20, "Микроплатформа")
-	L3 = (30, "Платформа")
-	L4 = (40, "Модель данных")
-	L5 = (50, "Модель событий")
-	L6 = (60, "Механика данных")
-	L7 = (70, "Механика управления")
-	L8 = (80, "Логика данных")
-	L9 = (90, "Логика управления")
+@dataclass
+class ClassInfo:
+	class_name: str
+	class_src : str
 
 
-MONTHS = ["",
-          "янв",
-          "фев",
-          "мар",
-          "апр",
-          "май",
-          "июн",
-          "июл",
-          "авг",
-          "сен",
-          "окт",
-          "ноя",
-          "дек"
-          ]
+class META_CLASSES(ClassInfo, enum.Enum):
+	C20_META_FRAME   = "C20_MetaFrame",             "G20_meta_frame"
+	C30_CACTUS_FRAME = "C30_StructFrame",           "G30_cactus_frame"
+	C31_CACTUS_FRAME = "C31_StructFrameWithEvents", "G31_cactus_frame"
+
+	C20_PYSIDE_APP   = "C20_PySideApplication",     "L20_PySide6"
+	C20_PYSIDE_FORM  = "C20_PySideForm",            "L20_PySide6"
+	C20_PYSIDE_FRAME = "C20_DiaFrame",              "L20_PySide6"
 
 
-baseclass   : str  = "C20_MetaFrame"    # Корневое/Родительское имя класса
-classname   : str  = "ClassName"        # Имя класса
-description : str  = "Описание класса"  # Описание класса
-flag_item   : bool = True               # Наличие элемента класса
-flag_single : bool = False              # Упаковка в едином классе
+# ПАРАМЕТРЫ ГЕНЕРАЦИИ
+# Мета-класс
+META_CLASS       = META_CLASSES.C20_META_FRAME
 
-dtime              = datetime.datetime.now()
-dy          : int  = dtime.year
-dm          : int  = dtime.month
-dd          : int  = dtime.day
+# Имя класса
+CLASS_NAME        = "SomethingCalculator"
 
-# Генерация упакованного класса в одном файле
-if flag_single:
-	filename = f"{classname.lower()}.py"
-	filepath = Path(f"./{filename}")
-	if filepath.exists():
-		print(f"{filename}: Файл существует, завершение.")
-		exit(0)
+# Описание класса
+CLASS_DESCRIPTION = "Калькулятор чего-то там"
 
-	with open(filepath, "w") as file:
-		data : list[str] = [f"# {description.upper()}",
-		                    f"# {dd:02d} {MONTHS[dm].upper()} {dy}",
-		                    f"\n"]
+# Имя файла
+# Если оставить пустым, сгенерируется автоматически из имени класса
+FILE_NAME         = ""
 
-		if flag_item: data.extend([
-		                    f"class C{classname}Item({baseclass}):",
-		                    f"\t\"\"\"{description}.Элемент\"\"\"",
-		                    f"",
-		                    f"\t# {LS.L4.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L5.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L6.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L7.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L8.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L9.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    ])
+# Корневая папка генерации файлов/директорий L
+DIR_ROOT          = "./"
 
-		data.extend([		f"class C{classname}({baseclass}):",
-		                    f"\t\"\"\"{description}\"\"\"",
-		                    f"",
-		                    f"\t# {LS.L4.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L5.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L6.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L7.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L8.description}",
-		                    f"\tpass"
-		                    f"\n"
-		                    f"\n"
-		                    f"\t# {LS.L9.description}",
-		                    f"\tpass"
-		                    ])
+# Генерация UI-Класса
+TARGET_IS_UI      = True
 
-		file.write('\n'.join(data))
-		print(f"{filename}: Генерация файла завершена.")
-		exit(0)
+# Генерация единого файла, без разделения на L4-9
+TARGET_IS_SINGLE  = False
 
 
-for L in [LS.L4, LS.L5, LS.L6, LS.L7, LS.L8, LS.L9]:
-	filename = f"L{L.level:02d}_{classname.lower()}.py"
-	filedir  = Path(f"./L{L.level//10}")
-	if not filedir.exists(): mkdir(filedir)
+# СЛУЖЕБНЫЕ ПАРАМЕТРЫ
+if not FILE_NAME: FILE_NAME = ''.join([f"_{ch}" if ch.isupper() else ch for ch in CLASS_NAME]).lower()
+FILE_NAME         = FILE_NAME[1:] if FILE_NAME[0] == '_' else FILE_NAME
 
-	filepath = Path(f"./L{L.level//10}/{filename}")
-	if filepath.exists():
-		print(f"{filename}: Файл существует, пропуск.")
-		continue
+LEVEL             = 0
+LEVELS            = [40, 50, 60, 70, 80, 90]
+if TARGET_IS_UI:
+	LEVELS.insert(1, 41)
+	LEVELS.insert(2, 42)
 
-	with open(filepath, "w") as file:
-		data : list[str] = [f"# {description.upper()}: {L.description.upper()}",
-		                    f"# {dd:02d} {MONTHS[dm].upper()} {dy}",
-		                    f""
-		                    ]
+DTIME             = datetime.datetime.now()
+DY                = DTIME.year
+DM                = DTIME.month
+DD                = DTIME.day
 
-		if L.level > 40: data.extend([
-							f"from L{L.level-10:02d}_{classname.lower()} import C{L.level - 10:02d}_{classname}{f", C{L.level-10:02d}_{classname}Item" if flag_item else ""}",
-							"\n"
-							])
+MONTHS            = ["", "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"]
 
 
-		if flag_item: data.extend([
-							f"class C{L.level:02d}_{classname}Item({baseclass if L.level == 40 else f"C{L.level-10:02d}_{classname}Item"}):",
-		                    f"\t\"\"\"{description}.Элемент: {L.description} \"\"\"",
-		                    f"\n"
-		                    f"\tpass",
-							f"\n"
-		                    ])
+def LevelToString() -> str:
+	""" Описание уровня """
+	match LEVEL:
+		case 40: return "Каркас UI" if TARGET_IS_UI else "Модель данных"
+		case 41: return "Модель UI"
+		case 42: return "Модель данных"
+		case 50: return "Модель событий"
+		case 60: return "Механика данных"
+		case 70: return "Механика управления"
+		case 80: return "Логика данных"
+		case 90: return "Логика управления"
+		case _ : return ""
 
-		data.extend([       f"class C{L.level:02d}_{classname}({baseclass if L.level == 40 else f"C{L.level - 10:02d}_{classname}"}):",
-							f"\t\"\"\"{description}: {L.description} \"\"\"",
-							f"\n"
-							f"\tpass"
-		])
 
-		file.write('\n'.join(data))
-		print(f"{filename}: Генерация файла завершена.")
+def GenerateSingleFile():
+	""" Генерация комплексного файла """
+	global LEVEL
+
+	FILE_PATH : Path      = Path(DIR_ROOT).joinpath(f"{FILE_NAME}.py")
+
+	data      : list[str] = []
+	data.append(f'# {CLASS_DESCRIPTION.upper()}')
+	data.append(f'# {DD:02d} {MONTHS[DM]} {DY:04d}')
+	data.append(f'')
+	data.append(f'from {META_CLASS.class_src} import {META_CLASS.class_name}')
+	data.append(f'')
+	data.append(f'')
+	data.append(f'class C_{CLASS_NAME}({META_CLASS.class_name}):')
+	data.append(f'\t""" {CLASS_DESCRIPTION} """')
+	data.append(f'')
+
+	for LEVEL in LEVELS:
+		data.append(f'\t# {LevelToString()}')
+		data.append(f'\tpass')
+		data.append(f'')
+
+	with open(FILE_PATH.absolute(), "w") as file: file.write('\n'.join(data))
+
+
+def GenerateFile():
+	""" Генерация файла """
+	global LEVEL
+
+	LEVEL_PATH    : Path      = Path(DIR_ROOT).joinpath(f"L{LEVEL//10}")
+	FILE_PATH     : Path      = LEVEL_PATH.joinpath(f"L{LEVEL:02d}_{FILE_NAME}.py")
+
+	idx_level     : int       = LEVELS.index(LEVEL)
+	PARENT_LEVEL  : int       = 0 if not idx_level else LEVELS[idx_level - 1]
+	PARENT_CLASS  : str       = META_CLASS.class_name if not PARENT_LEVEL else f"C{PARENT_LEVEL:02d}_{CLASS_NAME}"
+	PARENT_SRC    : str       = META_CLASS.class_src  if not PARENT_LEVEL else f"L{PARENT_LEVEL:02d}_{FILE_NAME}"
+
+	data          : list[str] = []
+	data.append(f'# {CLASS_DESCRIPTION.upper()}: {LevelToString().upper()}')
+	data.append(f'# {DD:02d} {MONTHS[DM]} {DY:04d}')
+	data.append(f'')
+	data.append(f'from {PARENT_SRC} import {PARENT_CLASS}')
+	data.append(f'')
+	data.append(f'')
+	data.append(f'class C{LEVEL:02d}_{CLASS_NAME}({PARENT_CLASS}):')
+	data.append(f'\t""" {CLASS_DESCRIPTION}: {LevelToString()} """')
+	data.append(f'\tpass')
+	data.append(f'')
+
+	if not LEVEL_PATH.exists(): mkdir(LEVEL_PATH)
+	with open(FILE_PATH, "w") as file: file.write('\n'.join(data))
+
+
+if TARGET_IS_SINGLE:
+	GenerateSingleFile()
+	exit(0)
+
+for LEVEL in LEVELS:
+	GenerateFile()
