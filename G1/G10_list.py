8761d911e20daf6b3b35d129ebb936ba927493a4
e_vladimir <79600069487@ya.ru>
2025-04-19 21:29:13 +0300

    * Перевод сравнения списков на множества

--- G1/G10_list.py
+++ G1/G10_list.py
@@ -1,7 +1,5 @@
 # ОБРАБОТЧИКИ СПИСКОВ
-# 29 мар 2025
-
-import re
+# 19 апр 2025
 
 
 def DistinctAndSortList1D(values: list, flag_distinct: bool = False, flag_sort: bool = False) -> list:
@@ -32,33 +30,9 @@ def DistinctAndSortList2D(values: list, index_processing_item: int, flag_distinc
 
 def DifferenceLists(list_1: list, list_2: list, flag_cmp_1_to_2: bool = False) -> list:
 	""" Разница между списками """
-	result = []
-
-	# TODO: Переписать на множествах
-
-	if flag_cmp_1_to_2:
-		for item in list_1:
-			if item in list_2: continue
-
-			result.append(item)
-
-	else:
-		for item in list_2:
-			if item in list_1: continue
-
-			result.append(item)
-
-	return result
+	return list(set(list_1 if flag_cmp_1_to_2 else list_2).difference(list_2 if flag_cmp_1_to_2 else list_1))
 
 
-def ClearList(items: list[str], clear_short: bool = True, clear_empty: bool = True, clear_spaces: bool = True, clear_numbers: bool = True, clear_simbols: bool = True, flag_sort: bool = True) -> list[str]:
-	""" Очистка списка от пустых строк, пробелов, чисел, спецсимволов """
-	result = items[:]
-
-	if clear_numbers: result = [re.sub(r'[0-9]',                 '', item) for item in result]
-	if clear_simbols: result = [re.sub(r'[^a-zA-Zа-яА-Я0-9\s.]', '', item) for item in result]
-	if clear_spaces : result = [item.strip().replace('  ', ' ')            for item in result]
-	if clear_empty  : result = filter(lambda item: len(item) > 0, result)
-	if clear_short  : result = filter(lambda item: len(item) > 3, result)
-
-	return sorted(result) if flag_sort else result
+def CLeanList(items: list[str]) -> list[str]:
+	""" Чистый список от пустых строк и пробелов """
+	return sorted(filter(lambda item: len(item) > 0, [item.strip() for item in items]))
